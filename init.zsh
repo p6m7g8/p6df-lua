26f71f1826d2b3be3ebf682973ad8fc9ed5f5172
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 22:18:35 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,13 @@
+p6df::modules::lua::version() { echo "0.0.1" }
+p6df::modules::lua::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::lua::external::() {
+}
+
+p6df::modules::lua::init() {
+
+}
+
+p6df::modules::lua::init
